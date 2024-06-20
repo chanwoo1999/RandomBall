95c5f0690d44c2574f0a7d2e57591024abb0df3d
kim-museong <antjd0419@gmail.com>
2024-06-20 19:38:45 +0900

    change ts

--- index.ts
+++ index.ts
@@ -1,38 +1,52 @@
-"use strict";
 // 전역변수 사용을 줄이기 위해 모듈화
 const BaseBallGame = (() => {
-    const startButton = document.querySelector('.start');
-    const submitButton = document.getElementById('submit');
-    const submitNumbers = document.querySelectorAll('.submit-number');
-    const modalBox = document.querySelector('.modal-background');
-    const modalBoxValue = document.querySelector('.modal > div');
-    const listBox = document.querySelector('.list');
-    const resultBox = document.getElementById('result');
-    let userNumberList = []; // 유저의 입력값들
-    let results = []; // 입력값에 따른 결과값들
-    let randomNumbers = [];
+    const startButton = document.querySelector('.start') as HTMLButtonElement;
+    const submitButton = document.getElementById('submit') as HTMLButtonElement;
+    const submitNumbers = document.querySelectorAll('.submit-number') as NodeListOf<HTMLInputElement>;
+    const modalBox = document.querySelector('.modal-background') as HTMLElement;
+    const modalBoxValue = document.querySelector('.modal > div') as HTMLElement;
+    const listBox = document.querySelector('.list') as HTMLElement;
+    const resultBox = document.getElementById('result') as HTMLElement;
+
+    interface ResultProps {
+        strike: number;
+        ball: number;
+    }
+
+    let userNumberList: Array<string> = []; // 유저의 입력값들
+    let results: Array<ResultProps> = []; // 입력값에 따른 결과값들
+    let randomNumbers: Array<number> = [];
     let attemptCount = 0;
+
     /** 랜덤번호 생성함수 */
     const generateRandomNumbers = () => {
         let firstNumber, secondNumber, thirdNumber;
+
         firstNumber = Math.floor(Math.random() * 10);
+
         do {
             secondNumber = Math.floor(Math.random() * 10);
         } while (firstNumber === secondNumber);
+
         do {
             thirdNumber = Math.floor(Math.random() * 10);
         } while (thirdNumber === firstNumber || thirdNumber === secondNumber);
+
         return [firstNumber, secondNumber, thirdNumber];
     };
+
     /** 번호 검증 함수 ( 랜덤번호배열, 사용자입력배열 ) */
-    const confirmNumber = (randomNumberArray, userInputArray) => {
+    const confirmNumber = (randomNumberArray: number[], userInputArray: number[]) => {
         let strike = 0;
         let ball = 0;
+
         strike = randomNumberArray.filter((v, i) => v === userInputArray[i]).length;
         ball = randomNumberArray.filter((v, i) => v !== userInputArray[i] && userInputArray.includes(v)).length;
+
         attemptCount++;
         return { strike, ball };
     };
+
     /** 리스트 업데이트 함수  */
     const updateList = () => {
         listBox.textContent = '';
@@ -46,11 +60,13 @@ const BaseBallGame = (() => {
             listBox.append(liElement);
         });
     };
+
     /** 모달창 변경 함수 */
-    const setModal = (text, buttonText) => {
+    const setModal = (text: string, buttonText: string) => {
         modalBoxValue.innerHTML = text;
         startButton.textContent = buttonText;
     };
+
     /** 설정값 초기화 함수 */
     const reset = () => {
         modalBox.classList.add('invisible');
@@ -61,11 +77,13 @@ const BaseBallGame = (() => {
         listBox.textContent = '';
         submitNumbers.forEach((el) => (el.value = ''));
     };
+
     startButton.addEventListener('click', () => {
         randomNumbers = generateRandomNumbers();
         console.log(randomNumbers); //개발용 콘솔 정답
         reset();
     });
+
     submitButton.addEventListener('click', () => {
         const submitNumbersArray = Array.from(submitNumbers).map((el) => +el.value);
         if (submitNumbersArray.some(isNaN)) {
@@ -81,6 +99,7 @@ const BaseBallGame = (() => {
                                     <div class='zone'><span class='ball b'>B</span><span>${ball}</span></div>
                                 `;
         updateList();
+
         if (strike === 3) {
             let text = ` <p>${attemptCount}번만에 정답을 맞추셨습니다.</p>
                              <p>게임이 종료되었습니다.</p> `;
@@ -89,8 +108,10 @@ const BaseBallGame = (() => {
             modalBox.classList.remove('invisible');
             return;
         }
+
         submitNumbers.forEach((el) => (el.value = ''));
     });
+
     // 입력값에 따른 버튼 활성화 설정
     submitNumbers.forEach((el) => {
         el.addEventListener('input', () => {
